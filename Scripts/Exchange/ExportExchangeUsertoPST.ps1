81baa75d8c08b43abaa9041f42402c4318e22964
DirkManhands <atubeofseries@gmail.com>
2016-06-08 15:05:45 -0400

    Update ExportExchangeUsertoPST.ps1

--- Scripts/Exchange/ExportExchangeUsertoPST.ps1
+++ Scripts/Exchange/ExportExchangeUsertoPST.ps1
@@ -1,6 +1,6 @@
-#Works only in Exchange 2010 and newer
+#THIS SCRIPT ONLY WORKS IN EXCHANGE 2010 AND OVER. 2007 REQUIRES A 32BIT WINDOWS MACHINE WITH OFFICE 07 ON IT.
+
 $User = Read-Host -Prompt 'Enter user email address'
 $Path = Read-Host -Prompt 'Enter PST Export Path'
-
 
 Export-Mailbox –Identity $User -PSTFolderPath $Path
